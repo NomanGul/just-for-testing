0bda9eb1aa67c29d1682a38594d02260f8579da4
nomangul <nomangul2001@gmail.com>
2019-03-30 18:35:41 +0500

    logo prop added

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import styles from "./styles.css";
 
 export default class ReactKHI extends Component {
   render() {
-    return (
+    return this.props.logo ? (
+      <img
+        src="https://s3.amazonaws.com/media-p.slid.es/uploads/905781/images/5335091/pasted-from-clipboard.png"
+        alt="reactkhi logo"
+        className={styles.image}
+      />
+    ) : (
       <div className={styles.main_container}>
         <img
           src="https://s3.amazonaws.com/media-p.slid.es/uploads/905781/images/5335091/pasted-from-clipboard.png"
